3dc70df4155785c9aedc91cc8abe558cad36b56e
Elijah Beach <elijahbeach@gmail.com>
2022-02-10 12:57:57 -0600

    GetByNameMethods update

--- MusicApi.Models/Label/LabelDetail.cs
+++ MusicApi.Models/Label/LabelDetail.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MusicApi.Models.Artist;
+using MusicApi.Models.Song;
 
 namespace MusicApi.Models.Label
 {
@@ -14,6 +16,10 @@ namespace MusicApi.Models.Label
         public int YearFounded { get; set; }
 
         public string Location { get; set; }
+
+        public List<ArtistListItem> Artists { get; set; }
+
+        public List<SongListItem> Songs { get; set; }
 
     }
 }
